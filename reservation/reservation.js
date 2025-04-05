73b7e914d1ec3d0fdb62638e0a26589ff95c7fb5
Charlotte2001 <chenpin1206@gmail.com>
2025-04-05 03:23:57 -0600

    adjust

--- reservation/reservation.js
+++ reservation/reservation.js
@@ -132,3 +132,15 @@ timeSelect.value = "";
   });
   
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const current = window.location.pathname;
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+const href = link.getAttribute("href");
+if (current.includes(href)) {
+  link.classList.add("active");
+}
+});
+});
